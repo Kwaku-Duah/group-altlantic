307209f3745d62929d75469bbd7f061796ad9965
ahmed.zubairu <bazubairu@st.ug.edu.gh>
2024-07-23 09:42:14 +0000

    test

--- src/main/java/com/example/demo/ServletInitializer.java
+++ src/main/java/com/example/demo/ServletInitializer.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+//This is the spring initializer
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
